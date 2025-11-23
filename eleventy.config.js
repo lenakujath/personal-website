1e1388162673386a6ded82bebd8dc7ccd9653210
lenakujath <l.kujath@gmx.de>
2025-11-23 10:28:39 +0100

    add font

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,3 +1,9 @@
-export default function(eleventyConfig) {
-  eleventyConfig.addPassthroughCopy("bundle.css");
+export default function (eleventyConfig) {
+	// Output directory: _site
+	eleventyConfig.addPassthroughCopy("bundle.css");
+
+	// Copy `/fonts/` to `_site/css/fonts/`
+	// Keeps the same directory structure.
+	eleventyConfig.addPassthroughCopy("fonts");
+
 };
