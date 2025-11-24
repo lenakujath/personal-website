a47c844a310629c59416af6d971e03871ffe7cd8
lenakujath <l.kujath@gmx.de>
2025-11-24 10:33:11 +0100

    improve folder structure

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,9 +1,10 @@
 export default function (eleventyConfig) {
 	// Output directory: _site
+	// Keeps the same directory structure.
 	eleventyConfig.addPassthroughCopy("bundle.css");
 
-	// Copy `/fonts/` to `_site/css/fonts/`
-	// Keeps the same directory structure.
 	eleventyConfig.addPassthroughCopy("fonts");
+
+	eleventyConfig.addPassthroughCopy("src");
 
 };
