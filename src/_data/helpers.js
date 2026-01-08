b2b28bd2b10196fa318fe03282306031ce948418
lenakujath <l.kujath@gmx.de>
2026-01-08 14:15:06 +0100

    minor cleanup

--- src/_data/helpers.js
+++ src/_data/helpers.js
@@ -11,15 +11,12 @@ export default {
 
   getLinkActiveState(itemUrl, pageUrl) {
     let response = "";
-
     if (itemUrl === pageUrl) {
       response = ' aria-current="page"';
     }
-
     if (itemUrl.length > 1 && typeof pageUrl === "string" && pageUrl.indexOf(itemUrl) === 0) {
       response += ' data-state="active"';
     }
-
     return response;
   },
 };
